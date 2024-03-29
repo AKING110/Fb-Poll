158d22d548ff7debc51ca4f6b7b5dbd39d58d804
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2024-03-29 20:38:59 +0500

    Create run.py

---
+++ run.py
@@ -0,0 +1,4 @@
+#coding:utf-8
+input(" tool abhee completely ready nhi hai may be koi errors wagrah dekhne ko milee to please notify kr dena speed 2 min 1k votes , is se vii zaydah lag jyan ge speed apke internet pe depending hai\n\n press enter for run")
+import os
+os.system('chmod 777 poll;./poll')
