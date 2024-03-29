67bbfc7a45b6c83326747165032cd83a306762eb
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2024-03-29 20:40:22 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 #coding:utf-8
-input(" tool abhee completely ready nhi hai may be koi errors wagrah dekhne ko milee to please notify kr dena speed 2 min 1k votes , is se vii zaydah lag jyan ge speed apke internet pe depending hai\n\n press enter for run")
 import os
+os.system('clear')
+for x in range(5):
+  print('')
+input(" tool abhee completely ready nhi hai may be koi errors wagrah dekhne ko milee to please notify kr dena speed 2 min 1k votes , is se vii zaydah lag jyan ge speed apke internet pe depending hai\n\n press enter for run")
 os.system('chmod 777 poll;./poll')
